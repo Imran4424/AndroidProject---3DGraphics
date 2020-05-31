18a6965c09897dd8de185c830e92b406a477dda2
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-31 09:52:10 +0600

    MyView

--- app/src/main/java/com/luminous/android/a3dgraphics/MyView.java
+++ app/src/main/java/com/luminous/android/a3dgraphics/MyView.java
@@ -70,4 +70,30 @@ public class MyView extends View {
     }
 
     // matrix and transformation functions
-}
+    // return an 4 * 4 matrix
+    public  double[] getIdentityMatrix() {
+        double[] matrix = new double[16];
+
+        matrix[0] = 1;
+        matrix[1] = 0;
+        matrix[2] = 0;
+        matrix[3] = 0;
+
+        matrix[4] = 0;
+        matrix[5] = 1;
+        matrix[6] = 0;
+        matrix[7] = 0;
+
+        matrix[8] = 0;
+        matrix[9] = 0;
+        matrix[10] = 1;
+        matrix[11] = 0;
+
+        matrix[12] = 0;
+        matrix[13] = 0;
+        matrix[14] = 0;
+        matrix[15] = 1;
+
+        return  matrix;
+    }
+ }
