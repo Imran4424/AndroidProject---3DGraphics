9b4a36555385dcb762d39887f6e8a68061fbdaf6
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-31 07:18:51 +0600

    MyView

--- app/src/main/java/com/luminous/android/a3dgraphics/MyView.java
+++ app/src/main/java/com/luminous/android/a3dgraphics/MyView.java
@@ -6,7 +6,7 @@ import android.view.View;
 
 public class MyView extends View {
     private Paint redPaint; // paint object for drawing lines
-    private Coordinate[] cubeVertices; // the vertices of 3D Cube
+    private Coordinate[] cubeVertices; // the vertices of 3D cube
 
     public MyView(Context context) {
         super(context);
