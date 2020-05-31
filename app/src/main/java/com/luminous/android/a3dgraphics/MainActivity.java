cee988ef02c97c280ed94b5ee0f6e7fe0a7dd4fd
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-31 10:21:59 +0600

    set content view

--- app/src/main/java/com/luminous/android/a3dgraphics/MainActivity.java
+++ app/src/main/java/com/luminous/android/a3dgraphics/MainActivity.java
@@ -5,10 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private MyView myView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        // hide title bar
+        getSupportActionBar().hide();
+
+        myView = new MyView(this);
+        setContentView(myView);
     }
+
+
 }
