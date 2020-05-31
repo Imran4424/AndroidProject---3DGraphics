c61eac2f6219353f38722f40df2f4b0dbfe78d64
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-31 07:04:40 +0600

    co ordinate

--- app/src/main/java/com/luminous/android/a3dgraphics/Coordinate.java
+++ app/src/main/java/com/luminous/android/a3dgraphics/Coordinate.java
@@ -17,7 +17,7 @@ public class Coordinate {
         this.w = w;
     }
 
-    // 
+    // to keep it as a homogeneous coordinate -> 
     public void normalise() {
         // ensure that w != 0
         if (w != 0) {
