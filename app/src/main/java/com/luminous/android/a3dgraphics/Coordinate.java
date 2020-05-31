da98a6551cd4b22d0e2e8506cab2d47fa3d7c0da
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-31 06:49:20 +0600

    co ordinate

--- app/src/main/java/com/luminous/android/a3dgraphics/Coordinate.java
+++ app/src/main/java/com/luminous/android/a3dgraphics/Coordinate.java
@@ -3,6 +3,7 @@ package com.luminous.android.a3dgraphics;
 public class Coordinate {
     public double x, y, z, w;
 
+    // create coordinate object with 0, 0, 0
     public Coordinate() {
         x = y = z = 0;
         w = 1;
