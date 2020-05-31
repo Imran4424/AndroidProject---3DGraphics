20bb25cf187a3cd536f19ffdf74416a3fe94a119
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-31 09:40:47 +0600

    MyView

--- app/src/main/java/com/luminous/android/a3dgraphics/MyView.java
+++ app/src/main/java/com/luminous/android/a3dgraphics/MyView.java
@@ -64,7 +64,8 @@ public class MyView extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
+        // draw objects on the screen
         super.onDraw(canvas);
-        
+
     }
 }
