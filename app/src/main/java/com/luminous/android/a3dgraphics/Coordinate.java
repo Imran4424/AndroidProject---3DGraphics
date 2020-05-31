a3fb65ca83683d851bdb780f9e7d8c74efcc9d14
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-31 06:47:13 +0600

    3d graphics

---
+++ app/src/main/java/com/luminous/android/a3dgraphics/Coordinate.java
@@ -0,0 +1,17 @@
+package com.luminous.android.a3dgraphics;
+
+public class Coordinate {
+    public double x, y, z, w;
+
+    public Coordinate() {
+        x = y = z = 0;
+        w = 1;
+    }
+
+    public Coordinate(int x, int y, int z, int w) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.w = w;
+    }
+}
