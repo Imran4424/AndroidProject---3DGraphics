18d5a32a5e783f419bc83133a6facef05aab0e41
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-31 07:15:50 +0600

    MyView

--- app/src/main/java/com/luminous/android/a3dgraphics/MyView.java
+++ app/src/main/java/com/luminous/android/a3dgraphics/MyView.java
@@ -1,6 +1,12 @@
 package com.luminous.android.a3dgraphics;
 
+import android.content.Context;
 import android.view.View;
 
 public class MyView extends View {
+    
+
+    public MyView(Context context) {
+        super(context);
+    }
 }
